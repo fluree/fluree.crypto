b98708cfed2a602a46ddbee11c676c2cf6f630da
Lillia P <lpodlog@flur.ee>
2020-01-09 13:20:10 -0500

    v0.3.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fluree.crypto "0.3.0"
+(defproject fluree.crypto "0.3.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
