3547ab023f949b69b7e98014b730a9a4078e831c
Wes Morgan <git@wesmorgan.me>
2021-12-16 16:00:21 -0700

    Update versions & conventions in docker stuff

--- script/run-in-docker.sh
+++ script/run-in-docker.sh
@@ -8,5 +8,6 @@ image=fluree/${PWD##*/}
 
 echo "Running in ${image} container..."
 
-docker build --quiet --tag ${image} .
-docker run --security-opt seccomp=docker-chrome-seccomp.json --rm ${image} "$@"
+export DOCKER_BUILDKIT=1
+docker build --quiet --load --tag "${image}" .
+docker run --security-opt seccomp=docker-chrome-seccomp.json --rm "${image}" "$@"
